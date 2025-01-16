c128cd709db25b93f87b99d2a42e98471f3776ad
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-01-16 17:23:20 +0100

    Added new data to show

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,54 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+interface Station {
+  Municipio: string;
+  Rótulo: string;
+  'C.P.': string;
+  Horario: string;
+  Latitud: string;
+  'Longitud (WGS84)': string;
+  'Precio Gasolina 95 E5': string;
+  'Precio Gasolina 98 E5': string;
+}
 
 export default function Home() {
   const [comunityCode, setComunityCode] = useState('');
-  const [stations, setStations] = useState<any[]>([]);
+  const [stations, setStations] = useState<Station[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
-  const fetchStations = async () => {
-    const response = await fetch(`/api/gas-stations/${comunityCode}`);
+  const fetchStations = async (url: string) => {
+    setLoading(true);
+    setError('');
 
-    if (!response.ok) {
-      console.error('Failed to fetch stations');
-      return;
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        console.error('Error al cargar las estaciones');
+        return;
+      }
+
+      const data = await response.json();
+      setStations(data.ListaEESSPrecio);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const data = await response.json();
-    console.log(data.ListaEESSPrecio);
-    setStations(data.ListaEESSPrecio);
+  useEffect(() => {
+    fetchStations('/api/gas-stations');
+  }, []);
+
+  const handleFetchStations = () => {
+    if (comunityCode) {
+      fetchStations(`/api/gas-stations/${comunityCode}`);
+    } else {
+      setError('Por favor, selecciona una Comunidad Autónoma');
+    }
   };
 
   return (
@@ -46,12 +79,66 @@ export default function Home() {
         <option value="18">Ceuta</option>
         <option value="19">Melilla</option>
       </select>
-      <button onClick={fetchStations}>Mostrar</button>
-      <ul>
-        {stations.map((station, index) => (
-          <li key={index}>{station.Municipio}</li>
-        ))}
-      </ul>
+      <button onClick={handleFetchStations} disabled={loading}>
+        {loading ? 'Cargando...' : 'Mostrar'}
+      </button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <table>
+        <thead>
+          <tr>
+            <th>Rótulo</th>
+            <th>C.P.</th>
+            <th>Municipio</th>
+            <th>Horario</th>
+            <th>Ir</th>
+            <th>Gasoleo A</th>
+            <th>Gasoleo Premium</th>
+            <th>Gasolina 95</th>
+            <th>Gasolina 98</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stations.map((station, index) => (
+            <tr key={index}>
+              <td>{station.Rótulo}</td>
+              <td>{station['C.P.']}</td>
+              <td>{station.Municipio}</td>
+              <td>{station.Horario}</td>
+              <td>
+                <a
+                  href={`https://www.google.es/maps/place/${station.Latitud.replace(
+                    ',',
+                    '.'
+                  )},${station['Longitud (WGS84)'].replace(',', '.')}`}
+                  target="blank"
+                >
+                  Google Maps
+                </a>
+              </td>
+              <td>
+                {station['Precio Gasoleo A']
+                  ? `${station['Precio Gasoleo A']}€`
+                  : ''}
+              </td>
+              <td>
+                {station['Precio Gasoleo Premium']
+                  ? `${station['Precio Gasoleo Premium']}€`
+                  : ''}
+              </td>
+              <td>
+                {station['Precio Gasolina 95 E5']
+                  ? `${station['Precio Gasolina 95 E5']}€`
+                  : ''}
+              </td>
+              <td>
+                {station['Precio Gasolina 98 E5']
+                  ? `${station['Precio Gasolina 98 E5']}€`
+                  : ''}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
