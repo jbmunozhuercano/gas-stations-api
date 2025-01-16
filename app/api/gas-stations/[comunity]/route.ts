7bb767a45797e3584e55299522db58e4e49c61b2
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-01-16 12:54:09 +0100

    Added minimal functionality to optain gas stations

---
+++ app/api/gas-stations/[comunity]/route.ts
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch';
+
+export async function GET(
+  req: Request,
+  { params }: { params: { comunity: string } }
+) {
+  const comunityCode = (await params).comunity;
+  const date = new Date();
+  date.setDate(date.getDate() - 1);
+  const yesterday = `${date.getDate()}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, '0')}-${date.getFullYear()}`;
+  const url = `https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestresHist/FiltroCCAA/${yesterday}/${comunityCode}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: 'Failed to fetch data' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,38 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+'use client';
+import { useState } from 'react';
 
 export default function Home() {
+  const [comunityCode, setComunityCode] = useState('');
+  const [stations, setStations] = useState<any[]>([]);
+
+  const fetchStations = async () => {
+    const response = await fetch(`/api/gas-stations/${comunityCode}`);
+
+    if (!response.ok) {
+      console.error('Failed to fetch stations');
+      return;
+    }
+
+    const data = await response.json();
+    console.log(data.ListaEESSPrecio);
+    setStations(data.ListaEESSPrecio);
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Home</h1>
-      </main>
+    <div>
+      <h1>Estaciones de servicio por Comunidad Autónoma</h1>
+      <input
+        type="text"
+        value={comunityCode}
+        onChange={(e) => setComunityCode(e.target.value)}
+        placeholder="Introduzca la Comunidad Autónoma"
+      />
+      <button onClick={fetchStations}>Fetch Stations</button>
+      <ul>
+        {stations.map((station, index) => (
+          <li key={index}>{station.Municipio}</li>
+        ))}
+      </ul>
     </div>
   );
 }
