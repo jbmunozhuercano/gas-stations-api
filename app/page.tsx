75482341cbf08073385ffe061aa2cbbc32bae2f2
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-01-16 12:59:22 +0100

    Changed input with select of communities

--- app/page.tsx
+++ app/page.tsx
@@ -21,13 +21,32 @@ export default function Home() {
   return (
     <div>
       <h1>Estaciones de servicio por Comunidad Autónoma</h1>
-      <input
-        type="text"
+      <select
         value={comunityCode}
         onChange={(e) => setComunityCode(e.target.value)}
-        placeholder="Introduzca la Comunidad Autónoma"
-      />
-      <button onClick={fetchStations}>Fetch Stations</button>
+      >
+        <option value="">Comunidad Autónoma</option>
+        <option value="01">Andalucía</option>
+        <option value="02">Aragón</option>
+        <option value="03">Asturias</option>
+        <option value="04">Baleares</option>
+        <option value="05">Canarias</option>
+        <option value="06">Cantabria</option>
+        <option value="07">Castilla-La Mancha</option>
+        <option value="08">Castilla y León</option>
+        <option value="09">Cataluña</option>
+        <option value="10">Valencia</option>
+        <option value="11">Extremadura</option>
+        <option value="12">Galicia</option>
+        <option value="13">Madrid</option>
+        <option value="14">Murcia</option>
+        <option value="15">Navarra</option>
+        <option value="16">País Vasco</option>
+        <option value="17">La Rioja</option>
+        <option value="18">Ceuta</option>
+        <option value="19">Melilla</option>
+      </select>
+      <button onClick={fetchStations}>Mostrar</button>
       <ul>
         {stations.map((station, index) => (
           <li key={index}>{station.Municipio}</li>
