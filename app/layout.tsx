25a86fbcf7817a527930f4f8aec5154dfa6aa6be
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2025-02-13 19:25:57 +0100

    Added SeoText module

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Smooch_Sans } from 'next/font/google';
 import './globals.css';
 import { Header } from './modules/Header/Header';
+import { SeoText } from './modules/SeoText/SeoText';
 
 const smoochSans = Smooch_Sans({
   variable: '--smooch-sans-font',
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <SeoText />
       </body>
     </html>
   );
