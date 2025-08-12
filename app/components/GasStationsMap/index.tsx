93b24f2c38f64f4004676cff8b2154d8968173e8
Jorge Muñoz Huércano <jorge.munoz.huercano@gmail.com>
2025-08-12 08:46:01 +0200

    Added new gas type selector

--- app/components/GasStationsMap/index.tsx
+++ app/components/GasStationsMap/index.tsx
@@ -33,28 +33,70 @@ interface GasStationsMapProps {
   center: LatLngExpression;
   showDistance: boolean;
   zoom: number;
+  priceKey: keyof GasStation;
+  averagePrice: number;
 }
+
+// Custom marker icons
+const greenIcon = new L.Icon({
+  iconUrl:
+    'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+const yellowIcon = new L.Icon({
+  iconUrl:
+    'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+const redIcon = new L.Icon({
+  iconUrl:
+    'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 export default function GasStationsMap({
   stations,
   center,
   showDistance,
   zoom,
+  priceKey,
+  averagePrice,
 }: GasStationsMapProps) {
   return (
     <MapContainer
       center={center}
       zoom={zoom}
       className={styles.mapContainer}
-      key={center.toString() + zoom} // Ensures map recenters and zooms when changed
+      key={center.toString() + zoom}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {stations.map((station, idx) => {
         const lat = parseFloat(station.Latitud.replace(',', '.'));
         const lon = parseFloat(station['Longitud (WGS84)'].replace(',', '.'));
-        if (isNaN(lat) || isNaN(lon)) return null;
+        const price = parseFloat(station[priceKey].replace(',', '.'));
+        if (isNaN(lat) || isNaN(lon) || isNaN(price)) return null;
+
+        const EPSILON = 0.001; // tolerance for floating point comparison
+
+        let icon = yellowIcon;
+        if (price < averagePrice - EPSILON) icon = greenIcon;
+        else if (price > averagePrice + EPSILON) icon = redIcon;
+        // If price === averagePrice, keep yellowIcon
+
         return (
-          <Marker key={idx} position={[lat, lon]}>
+          <Marker key={idx} position={[lat, lon]} icon={icon}>
             <Popup>
               <StationCard station={station} showDistance={showDistance} />
             </Popup>
