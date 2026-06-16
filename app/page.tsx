2ca3e25e541ca93db55dc753d9c06ed3ee6850a7
Jorge Muñoz Huércano <jorge.munoz.huercano@gmail.com>
2026-06-16 19:08:07 +0200

    style: reduce municipality zoom from 10 to 8

--- app/page.tsx
+++ app/page.tsx
@@ -193,7 +193,7 @@ export default function Home(): JSX.Element {
   const defaultZoom = 6;
   const regionZoom = 7;
   const locationZoom = 12;
-  const municipalityZoom = 10;
+  const municipalityZoom = 8;
 
   /**
    * Calculates the zoom level based on location or region.
